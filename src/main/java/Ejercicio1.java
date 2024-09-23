15c6f1ab8b3014a8eb93aec9bf567fc68a97a634
CarlosJavierValenzue <carlos.valenzuela.j92@gmail.com>
2024-09-23 14:22:19 +0200

    Commit inicial hebras

---
+++ src/main/java/Ejercicio1.java
@@ -0,0 +1,15 @@
+
+public class Ejercicio1 extends Thread{
+    char letters;
+
+    public Ejercicio1(char letters) {
+        this.letters = letters;
+    }
+
+    @Override
+    public void run(){
+        System.out.println("");
+
+    }
+
+}
